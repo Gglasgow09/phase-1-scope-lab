84e61199adf86b9d3668af603aa6c004bf6757be
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-02-22 23:29:44 -0500

    global scope

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'sally'
+
+function upperCaseCustomerName() {
+   return customerName = customerName.toUpperCase()
+};
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer(topPerformer) {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'mark'
+}
+
+
